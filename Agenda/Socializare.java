574e40ea584c697d0d93718b1910a254e37ae0eb
cristinabalanovici <77093727+cristinabalanovici@users.noreply.github.com>
2021-05-11 21:35:05 +0300

    Add files via upload

---
+++ Agenda/Socializare.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.ubbcluj.subex;
+
+/**
+ *
+ * @author crisb
+ */
+public interface Socializare {
+    void socializeaza(Persoana p);
+}
