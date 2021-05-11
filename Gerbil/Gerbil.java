56aa61ec3042aa78025efb7348b018fe8e9c9bbe
cristinabalanovici <77093727+cristinabalanovici@users.noreply.github.com>
2021-05-11 21:33:05 +0300

    Add files via upload

---
+++ Gerbil/Gerbil.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.ubbcluj.lab10;
+import java.util.*;
+/**
+ *
+ * @author crisb
+ */
+public class Gerbil {
+    int gerbil;
+    public Gerbil(int gerbilNumber) {
+        gerbil=gerbilNumber;
+    }
+    public void hop()
+    {
+        System.out.println(gerbil);
+    }
+
+}
